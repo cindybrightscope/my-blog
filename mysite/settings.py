908dfc283e0bbb76cc383d6989fbd31229495b5c
Cindy <cindy.chen@strategic-i.com>
2018-03-29 15:09:29 -0700

    Added Rest Framework Endpoints and setup AWS

--- mysite/settings.py
+++ mysite/settings.py
@@ -75,10 +75,21 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'podblog',
+        'USER': 'cindyc',
+        'PASSWORD': 'happycindy1002',
+        'HOST': 'pod2018blog.cmqecjm22sux.us-east-2.rds.amazonaws.com',
+        'PORT': '3306',
     }
 }
 
