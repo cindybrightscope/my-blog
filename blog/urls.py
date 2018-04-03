9b49b6259da0d24e263a1f924c0a72301a5b4c5d
Cindy <cindy.chen@strategic-i.com>
2018-04-03 16:50:26 -0700

    updated fo ngnix

--- blog/urls.py
+++ blog/urls.py
@@ -3,11 +3,11 @@ from . import views
 
 
 urlpatterns = [
-    #url(r'^$', views.post_list, name='post_list'),
-    #url(r'^post/(?P<pk>\d+)/$', views.post_detail, name='post_detail'),
-    url(r'^$', views.api_root, name='api_root'),
-    url(r'^post$', views.PostListAPI.as_view(), name='post_list'),
-    url(r'^post/(?P<pk>\d+)/$', views.PostDetailAPI.as_view(), name='post_detail'),
-    url(r'^comment/$', views.CommentListAPI.as_view(), name='comment_list'),
-    url(r'^comment/(?P<pk>\d+)/$', views.CommentDetailAPI.as_view(), name='comment_detail')
+    url(r'^$', views.post_list, name='post_list'),
+    url(r'^post/(?P<pk>\d+)/$', views.post_detail, name='post_detail'),
+    # url(r'^$', views.api_root, name='api_root'),
+    # url(r'^post$', views.PostListAPI.as_view(), name='post_list'),
+    # url(r'^post/(?P<pk>\d+)/$', views.PostDetailAPI.as_view(), name='post_detail'),
+    # url(r'^comment/$', views.CommentListAPI.as_view(), name='comment_list'),
+    # url(r'^comment/(?P<pk>\d+)/$', views.CommentDetailAPI.as_view(), name='comment_detail')
 ]
