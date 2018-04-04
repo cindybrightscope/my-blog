2dac1e2758cce4614a24f4b44886e2c06b3c1833
Cindy <cindy.chen@strategic-i.com>
2018-04-04 16:06:54 -0700

    Updated template and static file settings

--- mysite/settings.py
+++ mysite/settings.py
@@ -131,7 +131,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 #STATIC_URL = '/static/'
-STATIC_URL = '/home/ubuntu/my-blog/static/'
+STATIC_URL = '/home/ubuntu/my-blog/blog/static/'
 
 REST_FRAMEWORK = {
     # Use Django's standard `django.contrib.auth` permissions,
