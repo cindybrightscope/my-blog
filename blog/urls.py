1f4a389830c2268f0e1fbe167e2bd5ae863b812e
Cindy <cindy.chen@strategic-i.com>
2018-04-05 11:13:11 -0700

    added static files url to settings

--- blog/urls.py
+++ blog/urls.py
@@ -1,8 +1,7 @@
 from django.conf.urls import url
+from django.conf import settings
 from . import views
 from django.conf.urls.static import static
-from django.conf import settings
-
 
 
 urlpatterns = [
