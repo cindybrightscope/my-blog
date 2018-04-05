57b2984e238dcf91bf4675492572ba0be842a8cd
Cindy <cindy.chen@strategic-i.com>
2018-04-05 10:43:51 -0700

    added static files url to settings

--- blog/urls.py
+++ blog/urls.py
@@ -1,13 +1,16 @@
 from django.conf.urls import url
 from . import views
+from django.conf.urls.static import static
+from django.conf import settings
+
 
 
 urlpatterns = [
     url(r'^$', views.post_list, name='post_list'),
-    url(r'^post/(?P<pk>\d+)/$', views.post_detail, name='post_detail'),
+    url(r'^post/(?P<pk>\d+)/$', views.post_detail, name='post_detail')
     # url(r'^$', views.api_root, name='api_root'),
     # url(r'^post$', views.PostListAPI.as_view(), name='post_list'),
     # url(r'^post/(?P<pk>\d+)/$', views.PostDetailAPI.as_view(), name='post_detail'),
     # url(r'^comment/$', views.CommentListAPI.as_view(), name='comment_list'),
     # url(r'^comment/(?P<pk>\d+)/$', views.CommentDetailAPI.as_view(), name='comment_detail')
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
