294fdcee83058311927c08f69c0ab59345881d49
Cindy <cindy.chen@strategic-i.com>
2018-04-04 17:01:03 -0700

    Updated static file path

--- mysite/settings.py
+++ mysite/settings.py
@@ -134,7 +134,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'blog/static')]
+    os.path.join(BASE_DIR, 'blog')]
 
 REST_FRAMEWORK = {
     # Use Django's standard `django.contrib.auth` permissions,
